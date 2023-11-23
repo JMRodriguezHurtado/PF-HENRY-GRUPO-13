c2747b4703a5af441552eb3cd527d8e75742e7e8
JMRodriguezHurtado <jmrodhurtado1989@hotmail.com>
2023-11-23 13:28:55 -0600

    [UPDATE] products model

---
+++ server/src/models/products.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  sale: {
+    type: Number,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+},
+{
+  timestamps: true,
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
