67f4f97f576acbbcf4329aa0fb259992f5ad27a0
XliazZz <silentcd1912@gmail.com>
2023-11-23 17:08:03 -0300

    feat/add-model-admin

---
+++ server/src/models/Admin.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+
+const adminSchema = new Schema(
+  {
+    idUser: [{
+      type: Schema.ObjectId,
+      ref: 'User'
+    }],
+    store: {
+      type: String,
+      unique: true,
+      require: true
+    },
+    description: {
+      type: String,
+      unique: false,
+      require: true
+    },
+    contact: {
+      type: String,
+      unique: true,
+      require: true
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: false,
+    versionKey: false
+  }
+);
+
+userSchadminSchemaema.methods.softDelete = function() {
+  this.deleted = true;
+  return this.save();
+};
+
+module.exports = model("Admin", adminSchema);
