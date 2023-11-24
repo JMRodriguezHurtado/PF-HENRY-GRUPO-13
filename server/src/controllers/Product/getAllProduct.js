13278685687dcbb063afe52f76fad9468b5ac671
JMRodriguezHurtado <jmrodhurtado1989@hotmail.com>
2023-11-24 13:26:29 -0600

    [UPDATE] getAllProduct ahora hace un paginado desde el back al igual que filtros

--- server/src/controllers/Product/getAllProduct.js
+++ server/src/controllers/Product/getAllProduct.js
@@ -1,18 +1,58 @@
-const Product = require('../../models/Product');
+const Product = require("../../models/Product"); 
 
-const getAllProducts = async () => {
+const getAllProducts = async (req, res, next) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const startIndex = (page - 1) * limit;
+  const results = {};
+
+  const { name, brand, sale, price, category, quantity } = req.query;
+  const whereConditions = {};
+
+  if (name) {
+    whereConditions.name = new RegExp(name, 'i');
+  }
+
+  if (category) {
+    whereConditions.category = new RegExp(category, 'i');
+  }
+
+  if (sale === "sale") {
+    whereConditions.sale = {
+      $gt: 0,
+    };
+  }
+
+  if (sale === "no-sale") {
+    whereConditions.sale = 0;
+  }
+
   try {
-    const products = await Product.find({});
+    const sort = {};
+    if (price === "highest") {
+      sort.price = -1;
+    } else if (price === "lowest") {
+      sort.price = 1;
+    }
 
-    if (products.length === 0) {
-      throw new Error("No products found.");
+    const count = await Product.countDocuments(whereConditions);
+    results.info = {
+      page: page,
+      limit: limit,
+      total: count,
     };
 
-    return products;
-  } catch (error) {
-    console.error('Error fetching products:', error.message);
-    throw new Error('Unable to fetch products.');
-  };
+    const products = await Product.find(whereConditions)
+      .sort(sort)
+      .limit(limit)
+      .skip(startIndex);
+
+    results.results = products;
+    res.paginatedResults = results;
+    next();
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
 };
 
 module.exports = getAllProducts;
