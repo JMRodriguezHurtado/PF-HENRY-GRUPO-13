227736daff8cdc6e5c6226253a0e7893a6260664
XliazZz <silentcd1912@gmail.com>
2023-11-24 17:34:02 -0300

    added style to card

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Card from './components/Card';
+import Detail from './components/Detail';
+import Home from './views/Home';
+
 const App = () => (
-  <main className="relative">
-    NavBar
-    <section className="xl:padding-1 wide:padding-r padding-b">
-      Products
-    </section>
-  </main>
+  <>
+    <Routes>
+      <Route path='/card' element={<Card />} />
+      <Route path='/detail/:id' element={<Detail />} />
+      <Route path='/home' element={<Home />} />
+    </Routes>
+  </>
 )
 
 export default App;
--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,21 +1,14 @@
-
 import { NavLink } from 'react-router-dom';
 
-const Card = ({ price, img, name, description, _id }) => {
+const Card = ({ price, img, name, _id }) => {
   return (
-    <div>
-      <NavLink to={`/detail/${_id}`}>
-        <div>
-          <img src={img} alt={`${name} image`} />
-        </div>
-        <div>
-          <h2>{price}</h2>
-        </div>
-        <div>
-          <p>{description}</p>
-        </div>
-      </NavLink>
-    </div>
+    <NavLink className={'group bg-gray-100 shadow duration-150 hover:scale-105 hover:shadow-md'} to={`/detail/${_id}`}>      
+      <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7 '>
+        <img className='h-full w-full object-cover object-center group-hover:opacity-75' src={img} alt={`${name} image`} />
+      </div>
+          <h3 className='my-4 pl-4 font-bold text-black-500'>{name}</h3>
+          <p className='mb-4 ml-4 text-xl font-semibold text-gray-800'>{price}</p>
+    </NavLink>
   );
 };
 
