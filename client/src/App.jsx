55400750347d5af9168ec63452cea4547db6a15a
XliazZz <silentcd1912@gmail.com>
2023-11-25 00:33:04 -0300

    added style to createProduct

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Detail from './components/Detail';
-import Home from './views/Home';
+import CreateProduct from './components/CreateProduct';
 
 const App = () => (
   <>
     <Routes>
       <Route path='/detail/:id' element={<Detail />} />
-      <Route path='/home' element={<Home />} />
+      <Route path='/CreateProduct' element={<CreateProduct />} />
     </Routes>
   </>
 )
