29f6a5edbec6cd913c76d77771691da3cf45f7f9
XliazZz <silentcd1912@gmail.com>
2023-11-30 18:30:59 -0300

    agregue el envio de mail al registrarse, hice funcional el login y register

--- server/src/controllers/User/auth.js
+++ server/src/controllers/User/auth.js
@@ -1,6 +1,7 @@
 const { OAuth2Client } = require("google-auth-library");
 const User = require("../../models/User");
 const signTokens = require('../../middlewares/Tokens/signTokens');
+const sendVerifyEmail = require('../../middlewares/Email/sendVerifyEmail');
 
 const googleClientId = process.env.CLIENT_ID;
 const googleClient = new OAuth2Client(googleClientId);
@@ -12,18 +13,28 @@ const authenticateWithGoogle = async (token) => {
       audience: googleClientId,
     });
 
-    const { email, name } = ticket.getPayload();
+    const { email, name, picture } = ticket.getPayload();
 
     let user = await User.findOne({ email });
 
     if (!user) {
-      user = new User({ email, name});
+      user = new User({ email, name, img: picture || ''});
       await user.save();
-    }
+    };
 
     const { accessToken, refreshToken } = signTokens(user._id);
 
-    return { access: true, accessToken, refreshToken };
+    if (user.verify === false) {
+      await sendVerifyEmail(user.email, user._id);
+    };
+
+    const data = {
+      name: user.name,
+      img: picture,
+      message: 'Usuario creado con exito!.'
+    };
+
+    return { access: true, accessToken, refreshToken, data };
   } catch (error) {
     console.error("Error authenticating with Google:", error);
     throw { access: false, error: "Invalid Google token" };
