610745c3322e26f5b2487009c51f8aa5acdbcade
WagnerSmith <wagnersmith123@hotmail.com>
2023-12-03 18:49:33 -0500

    boludo

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,11 +1,11 @@
-import Card from "../Card"
+import Product from "./Product"
 
 
 const Products = ({products}) =>{
     return(
         <div className="grid grid-cols-1 m-5 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
               {products?.map((product) => (
-                <Card
+                <Product
                   key={product._id}
                   name={product.name}
                   price={product.price}
