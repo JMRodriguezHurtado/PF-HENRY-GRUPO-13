0164085f06d8cdd7864789f8dbc9e31d6b29d2a9
JMRodriguezHurtado <jmrodhurtado1989@hotmail.com>
2023-12-06 20:13:54 -0600

    [UPDATE] dashboard protegido

--- client/src/components/navBar.jsx
+++ client/src/components/navBar.jsx
@@ -1,22 +1,43 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Modal } from "antd";
+import { Button } from "antd";
 import AuthModal from "./AuthModal";
 import SearchBar from "./SearchBar";
 import LOGO from "../assets/LOGO.png";
 import { RiLogoutBoxRLine } from "react-icons/ri";
 import { LuShoppingCart } from "react-icons/lu";
 import { FaRegUser } from "react-icons/fa";
-import { clearData } from "../redux/actions";
+import { clearData, getUserByID } from "../redux/actions";
 import { useDispatch, useSelector } from "react-redux";
 import { MdOutlineSpaceDashboard } from "react-icons/md";
-
+import { jwtDecode } from "jwt-decode";
 
 const NavBar = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const isUserLoggedIn = !!localStorage.getItem("token");
+
+      if (isUserLoggedIn) {
+        const token = localStorage.getItem("token");
+        const decodedToken = jwtDecode(token);
+          console.log(decodedToken);
+        if (decodedToken) {
+          try {
+            await dispatch(getUserByID(decodedToken.id));
+          } catch (error) {
+            console.error("Error dispatching getUserByID:", error);
+          }
+        }
+      }
+    };
+
+    fetchData();
+
+  }, [dispatch]);
 
   const showModal = () => {
     setIsModalOpen(true);
@@ -28,15 +49,19 @@ const NavBar = () => {
 
   const handleLogout = () => {
     localStorage.removeItem("token");
-    localStorage.setItem("authModalShown", "false")
+    localStorage.setItem("authModalShown", "false");
     dispatch(clearData());
     navigate("/home");
   };
 
   const isUserLoggedIn = !!localStorage.getItem("token");
+  const userData = useSelector((state) => state.userDetail);
+  const isAdmin = userData && userData.Admin;
+  console.log(userData);
+  console.log(isAdmin);
 
   return (
-    <nav className="fixed top-0 w-full bg-blue1 shadow-md z-40 px-[5vw] flex items-center justify-between p-2" >
+    <nav className="fixed top-0 w-full bg-blue1 shadow-md z-40 px-[5vw] flex items-center justify-between p-2">
       <div className="flex items-center">
         <img
           className="bg-gray-50 rounded-lg w-[50px] h-[50px] cursor-pointer"
@@ -51,7 +76,7 @@ const NavBar = () => {
       </div>
 
       <div className="flex items-center space-x-4">
-        {isUserLoggedIn && (
+        {isUserLoggedIn && isAdmin && (
           <MdOutlineSpaceDashboard
             className="w-[50px] h-[50px] cursor-pointer hover:text-gray-100"
             onClick={() => navigate("/dashboard")}
@@ -66,19 +91,19 @@ const NavBar = () => {
           />
         )}
 
-        <LuShoppingCart 
+        <LuShoppingCart
           className="w-[40px] h-[40px] cursor-pointer hover:text-gray-100"
           onClick={() => navigate("/shoppingCart")}
           title="Cart"
         />
-        
+
         {isUserLoggedIn ? (
           <button
             className="hover:text-gray-100 text-gray4 font-pop-light text-xl bg-transparent border-none shadow-none navbutton"
             type="button"
             onClick={handleLogout}
           >
-            <RiLogoutBoxRLine className="w-[40px] h-[40px] cursor-pointer" title="Logout"/>
+            <RiLogoutBoxRLine className="w-[40px] h-[40px] cursor-pointer" title="Logout" />
           </button>
         ) : (
           <Button
@@ -91,7 +116,7 @@ const NavBar = () => {
         )}
       </div>
 
-      <AuthModal isOpen={isModalOpen} onClose={handleCancel} onLogout={handleLogout}/>
+      <AuthModal isOpen={isModalOpen} onClose={handleCancel} onLogout={handleLogout} />
     </nav>
   );
 };
