0c7df404a4124659ce1f84903b08ae16776b51ff
WagnerSmith <wagnersmith123@hotmail.com>
2023-12-07 22:32:29 -0500

    boludo

--- client/src/views/Admin/Inventory/Inventory.jsx
+++ client/src/views/Admin/Inventory/Inventory.jsx
@@ -2,20 +2,20 @@ import Header from "./Header";
 import Items from "./Items";
 
 const Inventory = ({ title, products }) => {
-    const titles = ["Nombre", "Precio", "Cantidad", "Publicado"];
+  const columnTitles = ["Nombre", "Precio", "Cantidad", "Publicado"];
 
-    return (
-        <div className="mx-4 mt-8">
-            <h2 className="text-2xl font-bold mb-4">{title}</h2>
-            <div className="overflow-x-auto">
-                <table className="min-w-full">
-                    <caption className="text-lg font-semibold p-2">{title}</caption>
-                    <Header titles={titles} />
-                    <Items products={products} />
-                </table>
-            </div>
-        </div>
-    );
+  return (
+    <div className="mx-4 mt-8">
+      <h2 className="text-2xl font-bold mb-4">{title}</h2>
+      <div className="overflow-x-auto">
+        <table className="min-w-full border border-gray-300">
+          <caption className="text-lg font-semibold p-2">{title}</caption>
+          <Header titles={columnTitles} />
+          <Items products={products} />
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Inventory;
